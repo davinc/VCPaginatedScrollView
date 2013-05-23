50f702ecac7b3d6b67857e64fa873b5a498543d7
Vinay Chavan <vinaychavan22@gmail.com>
2013-05-23 14:00:18 +0530

    Craeted Demo Project

---
+++ Demo/Demo/VCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCAppDelegate.h
+//  Demo
+//
+//  Created by Vinay Chavan on 23/05/13.
+//  Copyright (c) 2013 Vinay Chavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Vinay Chavan on 23/05/13.
+//  Copyright (c) 2013 Vinay Chavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCAppDelegate class]));
+	}
+}
